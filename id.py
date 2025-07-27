bad752a2f1ee4b4b9671782cfb7679f6d8a9261e
HackerGautham <gauthamkrishnanus04@gmail.com>
2025-07-27 12:07:19 +0530

    Add files via upload
    simple college id generator

---
+++ id.py
@@ -0,0 +1,19 @@
+#student ID card generator
+print("-------------------------Student ID card generator -------------------------")
+#enter the detailes of the student
+name=input("Enter the name of the student: ")
+college=input("Enter the college name: ")
+department=input("Enter the  department name: ")
+roll_no=int(input("Enter the roll number: "))
+phone_no=int(input("Enter the mob no: "))
+blood_group=input("Enter the blood group: ")
+address=input("Enter the address of the student: ")
+
+print("-------------------------student ID card-------------------------")
+print("Name   : ",name)
+print("College: ",college)
+print("Department: ",department)
+print("Roll No: ",roll_no)
+print("Phone No: ",phone_no)
+print("Blood Group: ",blood_group)
+print("Address: ",address)
